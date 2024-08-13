86eb22177293df85ab0e63487f493779f53470ba
reiniertc <reinier@taudin-chabot.nl>
2024-08-13 14:54:30 +0200

    Update __init__.py

--- custom_components/airkey/__init__.py
+++ custom_components/airkey/__init__.py
@@ -1,6 +1,21 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
-async def async_setup(hass, config):
-    """Setup the AirKey integration."""
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the AirKey component."""
     hass.data.setdefault(DOMAIN, {})
     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up AirKey from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")

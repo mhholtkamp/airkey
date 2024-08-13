a345515487e55fa4f5c398db13743defd7b84c54
reiniertc <reinier@taudin-chabot.nl>
2024-08-13 16:11:54 +0200

    Update __init__.py

--- custom_components/airkey/__init__.py
+++ custom_components/airkey/__init__.py
@@ -1,5 +1,6 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from .sensor import AirKeySensor
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the AirKey component."""
@@ -7,4 +8,12 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up AirKey from a config entry."""
-    return await async_setup_entry(hass, entry)
+    hass.data.setdefault('airkey', {})
+    
+    # Create the sensor instance and store it in the hass.data dictionary
+    hass.data['airkey'][entry.entry_id] = AirKeySensor(hass, entry)
+
+    # Setup the sensor
+    await hass.data['airkey'][entry.entry_id].async_setup()
+
+    return True

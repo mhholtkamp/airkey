b9b4ae9e9ac2291d279b14f4bdb9a6ed0fa242f9
reiniertc <reinier@taudin-chabot.nl>
2024-08-13 14:55:08 +0200

    Update const.py

--- custom_components/airkey/const.py
+++ custom_components/airkey/const.py
@@ -1,3 +1,4 @@
 DOMAIN = "airkey"
 SENSOR_NAME = "AirKey Events"
-API_URL = "https://integration.api.airkey.evva.com:443/cloud/v1/events?limit=1000"
+API_URL = "https://integration.api.airkey.evva.com:443/cloud/v1/events?limit=100"
+CONF_API_KEY = "api_key"

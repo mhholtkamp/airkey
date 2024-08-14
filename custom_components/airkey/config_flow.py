c083617eede997750aedd16e008d088f568890e6
reiniertc <reinier@taudin-chabot.nl>
2024-08-14 15:01:08 +0200

    Update config_flow.py

--- custom_components/airkey/config_flow.py
+++ custom_components/airkey/config_flow.py
@@ -1,40 +1,34 @@
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.const import CONF_API_KEY, CONF_SCAN_INTERVAL
 import voluptuous as vol
 
-from .const import DOMAIN, CONF_API_KEY, CONF_SCAN_INTERVAL
+from .const import DOMAIN, DEFAULT_REFRESH_RATE
 
 class AirkeyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Airkey."""
+    """Handle a config flow for Evva Airkey."""
 
     VERSION = 1
 
-    def __init__(self):
-        """Initialize the flow."""
-        self._api_key = None
-        self._scan_interval = 15
-
-    async def async_step_user(self, user_input=None) -> FlowResult:
+    async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        if user_input is not None:
-            self._api_key = user_input[CONF_API_KEY]
-            self._scan_interval = user_input.get(CONF_SCAN_INTERVAL, 15)
+        if user_input is None:
+            return self._show_config_form()
 
-            return self.async_create_entry(
-                title="Airkey Integration",
-                data={
-                    CONF_API_KEY: self._api_key,
-                    CONF_SCAN_INTERVAL: self._scan_interval,
-                },
-            )
+        return self.async_create_entry(title="Evva Airkey", data=user_input)
 
+    def _show_config_form(self, errors=None):
+        """Show the configuration form to edit location data."""
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_API_KEY): str,
-                    vol.Optional(CONF_SCAN_INTERVAL, default=15): int,
+                    vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_REFRESH_RATE): int,
                 }
             ),
+            errors=errors or {},
         )
+
+    async def async_step_import(self, import_data):
+        """Handle import from configuration.yaml."""
+        return await self.async_step_user(user_input=import_data)
